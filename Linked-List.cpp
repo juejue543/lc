970ca0fd8360a6ac6c4b82d9f8326f87ed1efb08
Judy Essam <107353757+juejue543@users.noreply.github.com>
2023-03-07 19:00:18 +0200

    Add files via upload

---
+++ Linked-List.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+    void insert(int data, ListNode* head){
+    ListNode* n = NULL;
+    n = new ListNode();
+    n->val = data;
+    if (head ==  NULL){
+        head = n;
+        n->next = NULL;
+        n->val = data;
+    }
+    else{
+        ListNode *temp = NULL;
+        temp = new ListNode();
+        temp = head;
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+        temp->next = n;
+        n->next = NULL;
+}}
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        vector<int>temp;
+        while(list1 != NULL){
+            temp.push_back(list1->val);
+            list1 = list1->next;
+        }
+        while(list2!= NULL){
+            temp.push_back(list2->val);
+            list2 = list2->next;
+        }
+
+        sort(temp.begin(), temp.end());
+
+        ListNode* ans = NULL;
+        ans = new ListNode();
+        for(int i = 0; i< temp.size(); i++){
+            insert(temp[i], ans);
+        }
+
+        return ans->next;
+
+
+
+
+
+        
+    }
+};
